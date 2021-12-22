a8e0d4871f0d8c22fbf14cb1af714a4e43dee5da
rigonomelandron <franciscorodriguezcontero@gmail.com>
2021-12-22 18:30:39 +0100

    Ultimo retoque a javaScript

--- js/codigo.js
+++ js/codigo.js
@@ -1,44 +1,46 @@
 var alumnosListos = [];
 var alumnosRegalados = [];
 function mostrarNombres() {
-    console.log(alumnosListos.length);
-    if (alumnosListos.length > 0){
-        resetear();
-    }
+    resetearSoft();
+    var alumnos = [];
     var nombres = document.querySelector("#nombres").value;
-    var alumnos = nombres.split(",");
-    
-   
-    while (alumnos.length != alumnosListos.length && alumnos.length > 1) {
-         
+    alumnos = nombres.split(",");
+    var alumnosListos = [];
+    var alumnosRegalados = [];
+
+    //verificar que se introduce mas de un nombre
+    if (alumnos.length <= 1) {
+        alert("Debe introducir más de un nombre");
+    }
+
+    for (index in alumnos) {
+
+        var alumno1 = alumnos[index];
+        console.log(index, alumno1);
+
         do {
             var aleatorio = Math.floor(Math.random() * alumnos.length);
-            var alumno1 = alumnos[aleatorio];
-            var aleatorio = Math.floor(Math.random() * alumnos.length);
             var alumno2 = alumnos[aleatorio];
+            console.log(alumnosRegalados.indexOf(alumno2))
+            console.log(alumno2);
 
-            if (alumnosListos.indexOf(alumno1) == -1 && alumnosRegalados.indexOf(alumno2) == -1 && alumno1 != alumno2) {
-                alumnosListos.push(alumno1);
-                alumnosRegalados.push(alumno2);
-                crearInputs(alumno1, alumno2);
-            }
+        } while (alumnosRegalados.indexOf(alumno2) != -1 || alumno1 == alumno2)
 
-        } while (alumnosListos.indexOf(alumno1) == -1);
+        if (alumnosRegalados.indexOf(alumno2) == -1) {
+            //console.log(nombre1, nombre2)
+            alumnosRegalados.push(alumno2);
+            crearInputs(alumno1, alumno2);
+        }
 
-   
-
-        /*  console.log(alumno1);
-         console.log(aleatorio);
-         console.log(alumnos); */
 
     }
 
 }
 function crearInputs(nombre1, nombre2 = "") {
-    
+    console.log(nombre1, nombre2)
     var contenedor = document.createElement("div");
     contenedor.setAttribute('class', 'd-flex flex-row contenedor');
-    
+
     var entrada = document.createElement("input");
     var entrada2 = document.createElement("input");
     entrada.setAttribute('type', 'text');
@@ -49,13 +51,26 @@ function crearInputs(nombre1, nombre2 = "") {
     entrada2.setAttribute('class', 'form-control m-2');
     contenedor.appendChild(entrada);
     contenedor.appendChild(entrada2);
-    console.log(contenedor);
     var parent = document.querySelector("#entradas");
     parent.appendChild(contenedor);
 
 }
-function resetear(){
-    location.reload();
-   
+function resetear() {
+    document.querySelector("#nombres").value = "";
+    var element = document.querySelector("#entradas");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }  // location.reload();
+
 }
+function resetearSoft() {
+    var element = document.querySelector("#entradas");
+    while (element.firstChild) {
+        element.removeChild(element.firstChild);
+    }  // location.reload();
+
+}
+
+
+
 
