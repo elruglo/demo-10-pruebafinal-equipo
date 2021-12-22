1cb8e6b451f856aea1ad3c49b9f6bef757874919
rigonomelandron <franciscorodriguezcontero@gmail.com>
2021-12-22 12:59:41 +0100

    Creacion javascript

--- js/codigo.js
+++ js/codigo.js
@@ -1,24 +1,51 @@
-if(document.addEventListener) {
-    document.addEventListener("DOMContentLoaded", inicio);
-} else {
-    alert("Por favor, actualice el navegador");
-}
-function inicio(){
-}
-function mostrarnombres(){
+function mostrarNombres() {
     var nombres = document.querySelector("#nombres").value;
-    var arrayNombres = nombres.split(",");
-    console.log(arrayNombres);
+    var alumnos = nombres.split(",");
+    var alumnosListos = [];
+    var alumnosRegalados = [];
+    console.log(alumnos);
+    while (alumnos.length != alumnosListos.length) {
 
-    for (var i=0; i<=arrayNombres.length-1; i++) {
-        arrayNombres[i] =arrayNombres[i].trim();
+        do {
+            var aleatorio = Math.floor(Math.random() * alumnos.length);
+            var alumno1 = alumnos[aleatorio];
+            var aleatorio = Math.floor(Math.random() * alumnos.length);
+            var alumno2 = alumnos[aleatorio];
+
+            if (alumnosListos.indexOf(alumno1) == -1 && alumnosRegalados.indexOf(alumno2) == -1 && alumno1 != alumno2) {
+                alumnosListos.push(alumno1);
+                alumnosRegalados.push(alumno2);
+                crearInputs(alumno1, alumno2);
+            }
+
+        } while (alumnosListos.indexOf(alumno1) == -1);
+
+
+
+        /*  console.log(alumno1);
+         console.log(aleatorio);
+         console.log(alumnos); */
+
     }
 
-    console.log(pickRandom(arrayNombres.length));
+}
+function crearInputs(nombre1, nombre2 = "") {
+
+    var contenedor = document.createElement("div");
+    contenedor.setAttribute('class', 'd-flex flex-row');
+    var entrada = document.createElement("input");
+    var entrada2 = document.createElement("input");
+    entrada.setAttribute('type', 'text');
+    entrada.setAttribute('value', nombre1.trim());
+    entrada.setAttribute('class', 'form-control m-2');
+    entrada2.setAttribute('type', 'text');
+    entrada2.setAttribute('value', nombre2.trim());
+    entrada2.setAttribute('class', 'form-control m-2');
+    contenedor.appendChild(entrada);
+    contenedor.appendChild(entrada2);
+    console.log(contenedor);
+    var parent = document.querySelector("#entradas");
+    parent.appendChild(contenedor);
 
 }
 
-function pickRandom(limite){
-    var azar = Math.floor(Math.random()*limite);
-    return azar;
-}
