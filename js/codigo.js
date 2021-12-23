a91b74150a3e7bbd088c9f067e0f31df9b6a5377
rigonomelandron <franciscorodriguezcontero@gmail.com>
2021-12-23 21:36:43 +0100

    Commit final

--- js/codigo.js
+++ js/codigo.js
@@ -1,75 +1,91 @@
-var alumnosListos = [];
-var alumnosRegalados = [];
+
+
+
 function mostrarNombres() {
     resetearSoft();
-    var alumnos = [];
     var nombres = document.querySelector("#nombres").value;
-    alumnos = nombres.split(",");
+    var alumnos = nombres.split(",").sort();
     var alumnosListos = [];
     var alumnosRegalados = [];
+    console.log(alumnos);
+    var alumnosRepes = nombresRepetidos(alumnos);
+    console.log(alumnosRepes);
+    if (!alumnosRepes) {
+        if (alumnos.length > 1) {
+            while (alumnos.length !== alumnosListos.length) {
 
-    //verificar que se introduce mas de un nombre
-    if (alumnos.length <= 1) {
-        alert("Debe introducir más de un nombre");
-    }
+                do {
+                    var aleatorio = Math.floor(Math.random() * alumnos.length);
+                    var alumno1 = alumnos[aleatorio];
+                    var aleatorio = Math.floor(Math.random() * alumnos.length);
+                    var alumno2 = alumnos[aleatorio];
 
-    for (index in alumnos) {
+                    if (alumnosListos.indexOf(alumno1) == -1 && alumnosRegalados.indexOf(alumno2) == -1 && alumno1 != alumno2) {
+                        alumnosListos.push(alumno1);
+                        alumnosRegalados.push(alumno2);
+                        crearInputs(alumno1, alumno2);
+                    }
 
-        var alumno1 = alumnos[index];
-        console.log(index, alumno1);
+                } while (alumnosListos.length !== alumnos.length);
 
-        do {
-            var aleatorio = Math.floor(Math.random() * alumnos.length);
-            var alumno2 = alumnos[aleatorio];
-            console.log(alumnosRegalados.indexOf(alumno2))
-            console.log(alumno2);
-
-        } while (alumnosRegalados.indexOf(alumno2) != -1 || alumno1 == alumno2)
-
-        if (alumnosRegalados.indexOf(alumno2) == -1) {
-            //console.log(nombre1, nombre2)
-            alumnosRegalados.push(alumno2);
-            crearInputs(alumno1, alumno2);
+            }
+            var nombres = document.querySelector("#nombres").value = "";
+        } else {
+            alert("debe Introducir como minimo dos nombres");
         }
-
-
+    } else {
+        alert("No puede introducir nombres repetidos ni espacios en blanco")
+        var nombres = document.querySelector("#nombres").value = "";
     }
-
 }
-function crearInputs(nombre1, nombre2 = "") {
-    console.log(nombre1, nombre2)
+
+function crearInputs(nombre1, nombre2) {
+
     var contenedor = document.createElement("div");
     contenedor.setAttribute('class', 'd-flex flex-row contenedor');
-
     var entrada = document.createElement("input");
     var entrada2 = document.createElement("input");
-    entrada.setAttribute('type', 'text');
-    entrada.setAttribute('value', nombre1.trim());
+    entrada.value = nombre1;
     entrada.setAttribute('class', 'form-control m-2');
     entrada2.setAttribute('type', 'text');
-    entrada2.setAttribute('value', nombre2.trim());
+    entrada2.value = nombre2;
     entrada2.setAttribute('class', 'form-control m-2');
     contenedor.appendChild(entrada);
     contenedor.appendChild(entrada2);
     var parent = document.querySelector("#entradas");
     parent.appendChild(contenedor);
 
-}
-function resetear() {
-    document.querySelector("#nombres").value = "";
-    var element = document.querySelector("#entradas");
-    while (element.firstChild) {
-        element.removeChild(element.firstChild);
-    }  // location.reload();
 
 }
+
 function resetearSoft() {
     var element = document.querySelector("#entradas");
     while (element.firstChild) {
         element.removeChild(element.firstChild);
-    }  // location.reload();
+    }
 
 }
+function resetear() {
+
+    location.reload();
+
+}
+function nombresRepetidos(alumnos) {
+    var contador;
+    for (var i = 0; i < alumnos.length; i++) {
+        if (alumnos[i] == "") {
+            return true
+        }
+        for (var j = 0; j < alumnos.length; j++) {
+            if (alumnos[i] == alumnos[j] && i != j) {
+                return true;
+            }
+        }
+    }
+
+}
+
+
 
 
 
